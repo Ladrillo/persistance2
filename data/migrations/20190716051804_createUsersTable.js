100c2e1d55a1af178e7cf9117a19b811196edcf3
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-16 05:22:56 -0400

    wip

---
+++ data/migrations/20190716051804_createUsersTable.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increment();
+    table.text('name', 128);
+  });
+};
+
+exports.down = function(knex) {
+  
+};
