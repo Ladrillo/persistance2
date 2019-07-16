470f557505b9846be538287b718361df6a2898b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-16 05:55:46 -0400

    wip

---
+++ data/seeds/01-users.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { name: 'Petar'},
+        { name: 'Wasiu'},
+        { name: 'Justinas', email: 'justinas@me.com'}
+      ]);
+    });
+};
