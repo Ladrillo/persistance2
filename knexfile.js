b4c3ffb89012ff010d08328130628b4fb4d8d224
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-16 05:05:39 -0400

    wip

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/database.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data'
+    },
+    seeds: {
+      directory: './data'
+    }
+  }
+};
